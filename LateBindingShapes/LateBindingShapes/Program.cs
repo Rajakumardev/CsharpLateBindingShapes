6501000e95e82fc21d5382f48cbc36f1438b0b0b
Rajakumardev <rajakumarofficial@gmail.com>
2017-07-10 22:32:30 +0530

    Final commit

--- LateBindingShapes/LateBindingShapes/Program.cs
+++ LateBindingShapes/LateBindingShapes/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace LateBindingShapes
 {
+	public class Shapes{
+		public virtual void Draw(){
+			Console.WriteLine("from base class");
+		}
+	}
+	class Circle:Shapes{
+		public override void Draw ()
+		{
+			Console.WriteLine ("Drawing Circle");
+			base.Draw ();
+		}
+	}
+	class Rectangle:Shapes{
+		public override void Draw ()
+		{
+			Console.WriteLine ("Drawing Rectangle");
+			base.Draw ();
+		}
+	}
+	class Triangle:Shapes{
+		public override void Draw ()
+		{
+			Console.WriteLine ("Drawing Triangle");
+			base.Draw ();
+		}
+	}
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			List<Shapes> shapes = new List<Shapes> ();
+			shapes.Add (new Circle ());
+			shapes.Add (new Rectangle ());
+			shapes.Add (new Triangle ());
+
+			foreach(Shapes shape in shapes)
+			{
+				shape.Draw ();
+			}
 		}
 	}
 }
